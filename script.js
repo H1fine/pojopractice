9e2be1a285624f13a2c5c514ef55321c284348fc
Jasmine Boyd <Boyd.jasmine45@gmail.com>
2025-07-30 20:39:36 -0500

    Step 4 completed - Practice Challenge Exercises

--- script.js
+++ script.js
@@ -1,115 +1,106 @@
-// // const pet = {
-// //     "name": "Buddy",
-// //     "species": "Dog",
-// //     "age": 5
-// // };
+const pet = {
+    "name": "Buddy",
+    "species": "Dog",
+    "age": 5
+};
 
-// // console.log(pet.name);
-// // console.log(pet.species);
-// // console.log(pet.age);
+console.log(pet.name);
+console.log(pet.species);
+console.log(pet.age);
 
-// // pet.age = 6;
-// // console.log(pet.age);
+pet.age = 6;
+console.log(pet.age);
 
-// // // TODO: What do you think each console.log will print?
-// // //It will print the name,species and age of the pet
+// TODO: What do you think each console.log will print?
+//It will print the name,species and age of the pet
 
-// // const book = {
-// //     "title": "JavaScript for Beginners",
-// //     "author": "Jane Doe",
-// //     "pages": 250,
-// //     "isPublished": true
-// // };
+const book = {
+    "title": "JavaScript for Beginners",
+    "author": "Jane Doe",
+    "pages": 250,
+    "isPublished": true
+};
 
-// // // TODO: List all the keys as a comment 
-// // // (title, author, pages,   isPublished)
-// // // TODO: List all the values as a comment 
-// // // (Javascript for Beginners, Jane Doe, 250, true)
+// TODO: List all the keys as a comment 
+// (title, author, pages,   isPublished)
+// TODO: List all the values as a comment 
+// (Javascript for Beginners, Jane Doe, 250, true)
 
-// // // Exercise 2: Create Your Own POJO
-// // const pet = {
-// //     "name": "Buddy",
-// //     "species": "Dog",
-// //     "age": 5
-// // };
+// Exercise 2: Create Your Own POJO
+const pet = {
+    "name": "Buddy",
+    "species": "Dog",
+    "age": 5
+};
 
-// // console.log(pet);
+console.log(pet);
 
-// // // TODO: What are the values for name, species, and age? Write as comments.
-// // //Buddy, Dog, 5
-
-// // const cableShow = {
-// //     "name": "The Nanny",
-// //     "created": "1993"
-// //     "starred": "Fran Drescher"
-
-// // };
-
-// // console.log(cableShow);
-
-// // //values: The Nanny, 1993, Fran Drescher
-
-// // const sprite = {
-// //     "name": "Hero",
-// //     "x": 100,
-// //     "y": 200
-// // };
-
-// // console.log(sprite);
-
-// // // TODO: What does this object represent? Write your explanation as a comment.
-// // //the object is the const sprite and it is described with the key-value pairs
-// // //There are three properties and each propery holds a key-value pair. The keys are name,x,y  and the values are Hero,100,200
-
-// // const car = {
-// //     "make": "Toyota",
-// //     "model": "Corolla",
-// //     "year": 2020,
-// //     "color": "Blue"
-// // };
-// // console.log(car);
-
-// // // TODO: Fix the syntax error.
-// // // TODO: Explain what was wrong in a comment. Mixing the comma behind value 2020
+// TODO: What are the values for name, species, and age? Write as comments.
+//Buddy, Dog, 5
 
 
-// // const sprite = {
-// //     "name": "Hero",
-// //     "x": 100,
-// //     "y": 200
-// // };
 
-// // console.log(sprite.x);
-// // sprite.x = 150;
-// // console.log(sprite.x);
+const sprite = {
+    "name": "Hero",
+    "x": 100,
+    "y": 200
+};
 
-// // sprite.y = 250;
-// // console.log(sprite.y);
+console.log(sprite);
 
-// // // TODO: Write your guesses for each console.log as comments.
-// // //sprite = 50
+// TODO: What does this object represent? Write your explanation as a comment.
+//the object is the const sprite and it is described with the key-value pairs
+//There are three properties and each propery holds a key-value pair. The keys are name,x,y  and the values are Hero,100,200
 
-// const book = {
-//     "title": "JavaScript for Beginners",
-//     "author": "Jane Doe",
-//     "pages": 300
-// };
+const car = {
+    "make": "Toyota",
+    "model": "Corolla",
+    "year": 2020,
+    "color": "Blue"
+};
+console.log(car);
 
-// console.log(300);
-// // TODO: Print the number of pages.
-// // TODO: Update pages to 300.
-// // TODO: Print pages again to confirm update.
+// TODO: Fix the syntax error.
+// TODO: Explain what was wrong in a comment. Mixing the comma behind value 2020
 
-// const pet = {
-//     "name": "Buddy",
-//     "species": "Dog",
-//     "age": 6
-// };
-// console.log("Buddy");
-// console.log(6);
-// // TODO: Print name and age.
-// // TODO: Change age to 6.
-// // TODO: Print age again.
+
+const sprite = {
+    "name": "Hero",
+    "x": 100,
+    "y": 200
+};
+
+console.log(sprite.x);
+sprite.x = 150;
+console.log(sprite.x);
+
+sprite.y = 250;
+console.log(sprite.y);
+
+// TODO: Write your guesses for each console.log as comments.
+//sprite = 50
+
+const book = {
+    "title": "JavaScript for Beginners",
+    "author": "Jane Doe",
+    "pages": 300
+};
+
+console.log(300);
+// TODO: Print the number of pages.
+// TODO: Update pages to 300.
+// TODO: Print pages again to confirm update.
+
+const pet = {
+    "name": "Buddy",
+    "species": "Dog",
+    "age": 6
+};
+console.log("Buddy");
+console.log(6);
+// TODO: Print name and age.
+// TODO: Change age to 6.
+// TODO: Print age again.
 
 const sprite = {
     "name": "Hero",
