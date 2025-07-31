8a5e564aeaef8e66465fc2a2377adc45e50c6e62
Jasmine Boyd <Boyd.jasmine45@gmail.com>
2025-07-30 19:38:10 -0500

    step 1 completed - look and guess

---
+++ script.js
@@ -0,0 +1,15 @@
+const pet = {
+    "name": "Buddy",
+    "species": "Dog",
+    "age": 5
+};
+
+console.log(pet.name);
+console.log(pet.species);
+console.log(pet.age);
+
+pet.age = 6;
+console.log(pet.age);
+
+// TODO: What do you think each console.log will print?
+//It will print the name,species and age of the pet
